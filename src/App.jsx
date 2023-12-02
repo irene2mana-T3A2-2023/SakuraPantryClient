a26dd62dd925ae4cf3ecab3a2b8ca589de3e5fd4
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-02 23:46:40 +1100

    install and setup nextui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from '@nextui-org/react';
+
 function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-400">
       Hello world!
+      <Button>Hello world</Button>
     </h1>
   );
 }
