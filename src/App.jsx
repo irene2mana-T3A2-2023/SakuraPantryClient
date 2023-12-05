d3755fa2a55450e270b51501a33ff48c26b5d2f1
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-05 16:46:49 +1100

    Implement an empty page for the Sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Search from './pages/Search';
 import Category from './pages/Category';
 import Product from './pages/Product';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/search' element={<Search />} />
       <Route path='/category/:slug' element={<Category />} />
       <Route path='/product/:slug' element={<Product />} />
+      <Route path='/sign-in' element={<SignIn />} />
     </Routes>
   );
 }
