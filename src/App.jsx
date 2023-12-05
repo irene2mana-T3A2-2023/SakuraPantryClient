fe49729a384f77de9764cb8548340b8bd1f86f37
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-05 15:31:07 +1100

    Implement an empty page for the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { Button } from '@nextui-org/react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='p-10'>
-      <Button color='primary' className='mr-10'>
-        Primary button
-      </Button>
-      <Button color='secondary'>Secondary button</Button>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
