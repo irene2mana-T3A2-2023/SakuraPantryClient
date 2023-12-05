4b7c4174e45894d24a127ff912f5be40784abc89
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-05 15:55:48 +1100

    Implement an empty page for the Category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import Category from './pages/Category';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/product/:slug' element={<Search />} />
+      <Route path='/category/:slug' element={<Category />} />
     </Routes>
   );
 }
---
+++ src/pages/Category.jsx
@@ -0,0 +1,9 @@
+function Category() {
+  return (
+    <div>
+      <p>This is the Category page</p>
+    </div>
+  );
+}
+
+export default Category;
