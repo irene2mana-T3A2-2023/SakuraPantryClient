7b04a8fcaaedcd951308fb92d4de9f952784cd78
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-05 15:01:39 +1100

    fix: Update scripts in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Button } from '@nextui-org/react';
 function App() {
   return (
     <div className='p-10'>
-      <Button color="primary" className="mr-10">Primary button</Button>
+      <Button color='primary' className='mr-10'>
+        Primary button
+      </Button>
       <Button color='secondary'>Secondary button</Button>
     </div>
   );
