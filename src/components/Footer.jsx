2ab56a7c77d0eb91228088219ade4c2977e275b1
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-11 15:29:30 +1100

    add background color for header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className='w-full bg-primary min-h-[300px]'>
       <div className='container mx-auto p-5 h-full w-full max-w-[1536px] flex-grow flex'>
-        <p>Footer will come here</p>
+        <p className='text-white'>Footer will come here</p>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,6 @@
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Button,
-  Image
-} from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Image } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/SakuraPantry.png';
+import logo from '../assets/images/SakuraPantryLogo.png';
 import useAuth from './Auth/useAuth';
 
 export default function Header() {
@@ -17,21 +10,23 @@ export default function Header() {
   console.log({ isAuthenticated });
 
   return (
-    <Navbar isBordered isBlurred maxWidth='2xl' className='max-w-full'>
+    <Navbar isBordered isBlurred maxWidth='2xl' className='max-w-full bg-primary'>
       <NavbarBrand>
         <Link to='/'>
           <Image src={logo} className='h-[50px]' />
         </Link>
       </NavbarBrand>
       <NavbarContent className='hidden sm:flex gap-4' justify='center'>
-        <p>Search bar will go here</p>
+        <p className='text-white'>Search bar will come here</p>
       </NavbarContent>
       <NavbarContent justify='end'>
         <NavbarItem className='hidden lg:flex'>
-          <Link to='/sign-in'>Sign In</Link>
+          <Button as={Link} color='secondary' variant='solid' to='/sign-in'>
+            Sign In
+          </Button>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color='primary' to='/sign-up' variant='flat'>
+          <Button as={Link} color='secondary' variant='light' to='/sign-up'>
             Sign Up
           </Button>
         </NavbarItem>
