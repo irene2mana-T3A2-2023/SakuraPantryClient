6550462cf9825ed203e5e980dd3f8dec8884f3c6
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-11 20:43:13 +1100

    feat: footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,57 @@
+import React from 'react';
+import { Card, CardHeader, CardBody, Input, Button } from '@nextui-org/react';
+import { FaSquareInstagram } from 'react-icons/fa6';
+import { FaSquareFacebook } from 'react-icons/fa6';
+import { AiOutlineMail } from 'react-icons/ai';
+
 export default function Footer() {
   return (
-    <div className="w-full bg-primary min-h-[300px]">
+    <div className='w-full bg-primary min-h-[300px]'>
       <div className='container mx-auto p-5 h-full w-full max-w-[1536px] flex-grow flex'>
-        <p>Footer will come here</p>
+        {/*Text components*/}
+        <div className='flext-col md:flex'>
+          <Card className='w-full bg-primary shadow-none '>
+            <CardHeader className='flex gap2'>
+              <p className='text-secondary'>ABOUT US</p>
+            </CardHeader>
+            <CardBody>
+              <p className='text-white text-justify'>
+                Sakura Pantry is dedicated to delivering the highest quality Japanese ingredients to
+                homes across Australia. We take pride in offering carefully selected items, and we
+                are delighted to assist you in recreating your favorite Japanese dishes with a touch
+                of luxury in the comfort of your own home.
+              </p>
+            </CardBody>
+          </Card>
+
+          {/*Contact components*/}
+          <div className='flex w-full flex-col gap-5 md:ml-10 lg:ml-20'>
+            <p className='text-secondary'>Contact Us</p>
+            <Input
+              type='text'
+              label='Your name'
+              className='h-8 '
+              startContent={<AiOutlineMail className='text-1xl text-default-400' />}
+            />
+            <Input
+              type='email'
+              label='Your email'
+              className='h-8 '
+              startContent={<AiOutlineMail className='text-1xl text-default-400' />}
+            />
+            <Input type='text' label='Questions' className='h-16 ' />
+
+            {/*SNS and button components*/}
+            <div className='flex'>
+              <FaSquareFacebook className='text-blue-500 w-8 h-8 mr-5' />
+              <FaSquareInstagram className='text-pink-500 w-8 h-8' />
+              <Button className='text-#b8bfdb w-8 h-8 font-bold py-2 px-4 ml-20 md:ml-auto'>
+                Send
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
