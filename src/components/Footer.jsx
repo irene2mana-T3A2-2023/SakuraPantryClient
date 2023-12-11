fddb5f2edde75c4b8bd347e81cc1f7d17d54729f
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-12 10:53:39 +1100

    fix: button background colour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,7 +47,7 @@ export default function Footer() {
             <div className='flex'>
               <FaSquareFacebook className='text-[#4267B2] w-8 h-8 mr-5' />
               <FaSquareInstagram className='text-[#E4405F] w-8 h-8' />
-              <Button className='text-bg-secondary w-8 h-8 font-bold py-2 px-4 ml-auto'>
+              <Button className='text-#b8bfdb bg-secondary w-8 h-8 font-bold py-2 px-4 ml-auto'>
                 Send
               </Button>
             </div>
