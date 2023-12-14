d3ef5c42336f3470ae87e734adf89a502a7abadf
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-14 13:41:38 +1100

    implement slider effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import api from '../configs/api';
 import { useState, useEffect } from 'react';
 import homeImage from '../assets/images/homePage.jpg';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
   const [newArrivals, setNewArrivals] = useState([]);
@@ -36,82 +37,84 @@ const HomePage = () => {
         //Shuffle products.
         const shuffledProducts = shuffleArray(products);
         // Updates the newArrivals and featuredProducts state variables.
-        setNewArrivals(shuffledProducts.slice(0, 4));
+        setNewArrivals(shuffledProducts);
         // Fetch only products with isFeatured is true by filtering the products.
-        setFeaturedProducts(products.filter((product) => product.isFeatured).slice(0, 4));
+        setFeaturedProducts(products.filter((product) => product.isFeatured));
       } catch (error) {
-        //eslint-disable-next-line
-        console.error('Error fetching products', error);
+        // If there is an error during the registration process, catches the error and displays an error message.
+        toast.error('Error fetching products', error);
       }
     };
 
     fetchProducts();
   }, []);
 
-  // Framer motion settings
-  const variants = {
-    hidden: { opacity: 0, x: -100 },
-    visible: { opacity: 1, x: 0 }
-  };
-
   return (
     <Layout>
-      <div className='w-full mb-4'>
-        <img src={homeImage} alt='HomeImage' className='w-full object-cover h-64' />
+      <div className='w-full mb-10'>
+        <img src={homeImage} alt='HomeImage' className='w-full object-cover h-80' />
         {/*New Arrivals Section*/}
-        <div className='flex flex-col w-full'>
-          <h1 className='text-left font-bold text-2xl mt-10 mb-4 text-secondary'>New Arrivals</h1>
-          <motion.div
-            className='w-full mx-auto'
-            initial='hidden'
-            animate='visible'
-            variants={variants}
-          >
-            <div className='gap-10 grid grid-cols-2 sm:grid-cols-4'>
-              {/* Executes a given function for each element of the array, creating a new array from the results. */}
+        <div className='flex flex-col'>
+          <h1 className='text-left font-bold text-3xl mt-10 mb-4 text-primary'>New Arrivals</h1>
+          <div className='carousel mt-4 overflow-hidden'>
+            <motion.div
+              className='flex space-x-2'
+              drag='x'
+              dragConstraints={{ left: -1000, right: 0 }}
+            >
               {newArrivals.map((product, index) => (
-                // provide a unique identifier for each element, helping React optimize rendering performance and card is clickable.
-                <Card shadow='sm' key={index} isPressable>
-                  <CardBody className='flex justify-center items-center p-0 overflow-visible'>
+                <Card
+                  shadow='sm'
+                  key={index}
+                  isPressable
+                  className='border-2 w-[300px] h-[450px] flex-shrink-0'
+                >
+                  <CardBody className='flext justify-center overflow-visible'>
                     <Image
                       radius='lg'
-                      width='80%'
+                      width='100%'
                       alt={product.name}
-                      className='w-full object-cover h-[250px]'
+                      className='w-full object-contain'
                       src={product.imageUrl}
                     />
                   </CardBody>
-                  <CardFooter className='text-small justify-between'>
+                  <CardFooter className='text-small justify-around'>
                     <b>{product.name}</b>
                     <p className='text-default-500'>${product.price}</p>
                   </CardFooter>
                 </Card>
               ))}
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
+        </div>
 
-          {/* Featured Collection Section */}
-          <h1 className='text-left font-bold text-2xl mb-4 mt-8 text-secondary '>
+        {/* Featured Collection Section */}
+        <div className='flex flex-col mt-10'>
+          <h1 className='text-left font-bold text-3xl mb-4 mt-5 text-primary '>
             Featured Collection
           </h1>
-          <motion.div
-            className='w-full mx-auto'
-            initial='hidden'
-            animate='visible'
-            variants={variants}
-          >
-            <div className='gap-10 grid grid-cols-2 sm:grid-cols-4 mb-6'>
+          <div className='carousel mt-4 overflow-hidden'>
+            <motion.div
+              className='flex space-x-2'
+              drag='x'
+              dragConstraints={{ left: -1000, right: 0 }}
+            >
               {/*creates a new array including only products where the isFeatured property is true. It's a conditional check that filters out any products that don't meet this criterion. */}
               {featuredProducts
                 .filter((product) => product.isFeatured)
                 .map((product, index) => (
-                  <Card shadow='sm' key={index} isPressable>
-                    <CardBody className='flex justify-center items-center p-0 overflow-visible'>
+                  <Card
+                    shadow='sm'
+                    key={index}
+                    isPressable
+                    className='border-2 w-[300px] h-[450px] flex-shrink-0'
+                  >
+                    <CardBody className='flex justify-center overflow-visible h-full'>
                       <Image
                         radius='lg'
-                        width='85%'
+                        width='100%'
                         alt={product.name}
-                        className='w-full object-cover h-[250px]'
+                        className='w-full object-contain'
                         src={product.imageUrl}
                       />
                     </CardBody>
@@ -121,8 +124,8 @@ const HomePage = () => {
                     </CardFooter>
                   </Card>
                 ))}
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
         </div>
       </div>
     </Layout>
