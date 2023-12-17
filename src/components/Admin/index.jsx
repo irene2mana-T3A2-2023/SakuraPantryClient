caf002bb4041def79bcfa0dde21ec2f3fde79864
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-18 10:16:01 +1100

    Implement admin dashboard summary page (#18)
    * update readme
    
    * update readme
    
    * update readme
    
    * implement tab menu for admin dashboard page
    
    * hide search bar and cart logo in the admin layout header
    
    * implement search param for tab menu
    
    * implement summary content with hard code

---
+++ src/components/Admin/index.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Tabs, Tab } from '@nextui-org/react';
+import { FiDatabase, FiUser, FiEdit, FiPackage, FiFile } from 'react-icons/fi';
+import { useSearchParams } from 'react-router-dom';
+import Summary from './Summary';
+
+export default function AdminDashboard() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const onSelectionChange = (key) => {
+    setSearchParams({ tab: key });
+  };
+
+  return (
+    <div className='flex w-full flex-col h-11 min-w-lg'>
+      <Tabs
+        onSelectionChange={onSelectionChange}
+        defaultSelectedKey={searchParams.get('tab') || 'total-summary'}
+        aria-label='admin-tabs'
+        variant='bordered'
+        color='secondary'
+        className='w-full mt-4'
+        radius='sm'
+        classNames={{
+          base: 'h-[100px] w-full',
+          tabList: 'p-0 md:p-4 w-full border-0 md:border-1 shadow-none',
+          tab: 'h-full md:py-4',
+          tabContent: 'text-sm md:text-xl font-light'
+        }}
+      >
+        <Tab
+          key='total-summary'
+          title={
+            <div className='flex items-center space-x-2'>
+              <FiDatabase />
+              <span>Total Summary</span>
+            </div>
+          }
+        >
+          <div className='container max-w-full mt-10'>
+            <Summary />
+          </div>
+        </Tab>
+        <Tab
+          key='order-management'
+          title={
+            <div className='flex items-center space-x-2'>
+              <FiEdit />
+              <span>Orders</span>
+            </div>
+          }
+        >
+          <div className='container max-w-full mt-10'>This is order management table</div>
+        </Tab>
+        <Tab
+          key='product-management'
+          title={
+            <div className='flex items-center space-x-2'>
+              <FiPackage />
+              <span>Products</span>
+            </div>
+          }
+        >
+          <div className='container max-w-full mt-10'>This is products management table</div>
+        </Tab>
+        <Tab
+          key='categories-management'
+          title={
+            <div className='flex items-center space-x-2'>
+              <FiFile />
+              <span>Categories</span>
+            </div>
+          }
+        >
+          <div className='container max-w-full mt-10'>This is categories management table</div>
+        </Tab>
+        <Tab
+          key='users-management'
+          title={
+            <div className='flex items-center space-x-2'>
+              <FiUser />
+              <span>Users</span>
+            </div>
+          }
+        >
+          <div className='container max-w-full mt-10'>This is users management table</div>
+        </Tab>
+      </Tabs>
+    </div>
+  );
+}
