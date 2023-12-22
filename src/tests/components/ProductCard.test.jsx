13fb0c19105fd590e92294c152e6d573e8056bab
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-12-22 21:11:33 +1100

    finish test for ProductCard component

---
+++ src/tests/components/ProductCard.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import ProductCard from '../../components/ProductCard';
+
+describe('ProductCard Component', () => {
+  it('renders product details', () => {
+    const mockProduct = {
+      slug: 'miso-paste',
+      imageUrl: 'image-url.jpg',
+      name: 'Miso Paste',
+      category: { name: 'Sauces&Seasonings' },
+      price: 9.5
+    };
+
+    render(
+      <BrowserRouter>
+        <ProductCard product={mockProduct} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText('Miso Paste')).toBeInTheDocument();
+    expect(screen.getByText('Sauces&Seasonings')).toBeInTheDocument();
+    expect(screen.getByText('Price: $9.50')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src', 'image-url.jpg');
+  });
+});
