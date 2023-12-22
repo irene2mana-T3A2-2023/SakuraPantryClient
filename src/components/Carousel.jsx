6b76f23cdc868f5030a4e95c3d3311e6813aff58
Mana Misumi <mana.misumi3050@gmail.com>
2023-12-22 12:14:36 +1100

    implement slider effect for home image

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,56 @@
+// Carousel.js
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const Carousel = ({ items }) => {
+  const [page, setPage] = useState(0);
+  const numItems = items.length;
+  const delay = 3000; // Delay in milliseconds for auto-play
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPage((prev) => (prev + 1) % numItems);
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [numItems, delay]);
+
+  const variants = {
+    enter: { opacity: 0, x: 100 },
+    center: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -100 }
+  };
+
+  return (
+    <div className='relative w-full h-[150px] md:h-[300px] lg:h-[500px] overflow-hidden rounded-2xl mt-2'>
+      <AnimatePresence>
+        <motion.img
+          key={page}
+          src={items[page].imageUrl}
+          alt={items[page].title}
+          initial='enter'
+          animate='center'
+          exit='exit'
+          variants={variants}
+          transition={{
+            x: { type: 'tween', duration: 1 }, // Set transition duration for x-axis movement
+            opacity: { duration: 1 } // Set transition duration for opacity change
+          }}
+          className='absolute w-full h-full object-cover'
+        />
+      </AnimatePresence>
+      <div className='absolute bottom-0 w-full flex justify-center p-4'>
+        {items.map((_, index) => (
+          <span
+            key={index}
+            className={`mx-2 h-2 w-2 bg-secondary rounded-full ${
+              index === page ? 'bg-opacity-100' : 'bg-opacity-50'
+            }`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import Layout from '../layouts/Base';
 import api from '../configs/api';
 import { useState, useEffect } from 'react';
-import homeImage from '../assets/images/homePage.jpg';
 import toast from 'react-hot-toast';
 import ProductCardList from '../components/ProductCardList';
+import panel1 from '../assets/images/panel1.jpg';
+import panel2 from '../assets/images/panel2.jpg';
+import panel3 from '../assets/images/panel3.jpg';
+import panel4 from '../assets/images/panel4.jpg';
+import Carousel from '../components/Carousel';
 
+const items = [
+  { id: 1, imageUrl: panel1, title: 'Item 1' },
+  { id: 2, imageUrl: panel2, title: 'Item 2' },
+  { id: 3, imageUrl: panel3, title: 'Item 3' },
+  { id: 4, imageUrl: panel4, title: 'Item 4' }
+];
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [newArrivals, setNewArrivals] = useState([]);
@@ -35,8 +45,8 @@ const HomePage = () => {
 
   return (
     <Layout>
-      <div className='w-full mb-10'>
-        <img src={homeImage} alt='HomeImage' className='w-full object-cover h-50 lg:h-80' />
+      <div className='container relative max-w-full flex flex-col w-full mb-10'>
+        <Carousel items={items} />
         {/*New Arrivals Section*/}
         <ProductCardList products={newArrivals} title='New Arrivals' isLoading={isLoading} />
         {/* Featured Collection Section */}
