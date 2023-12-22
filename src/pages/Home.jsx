ce70af489e015ef90841b4c80fcabb239af1d9c6
Mana Misumi <126526974+Mana12011207@users.noreply.github.com>
2023-12-22 13:49:04 +1100

    Implement slider effect (#30)
    * implement slider effect for home image
    
    * change image
    
    * Make sns icons clickable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import Layout from '../layouts/Base';
 import api from '../configs/api';
 import { useState, useEffect } from 'react';
-import homeImage from '../assets/images/homePage.jpg';
 import toast from 'react-hot-toast';
 import ProductCardList from '../components/ProductCardList';
+import panel1 from '../assets/images/panel1.jpg';
+import panel2 from '../assets/images/panel2.png';
+import panel3 from '../assets/images/panel3.jpg';
+import Carousel from '../components/Carousel';
 
+const items = [
+  { id: 1, imageUrl: panel1, title: 'Item 1' },
+  { id: 2, imageUrl: panel2, title: 'Item 2' },
+  { id: 3, imageUrl: panel3, title: 'Item 3' }
+];
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [newArrivals, setNewArrivals] = useState([]);
@@ -35,8 +43,8 @@ const HomePage = () => {
 
   return (
     <Layout>
-      <div className='w-full mb-10'>
-        <img src={homeImage} alt='HomeImage' className='w-full object-cover h-50 lg:h-80' />
+      <div className='container relative max-w-full flex flex-col w-full mb-10'>
+        <Carousel items={items} />
         {/*New Arrivals Section*/}
         <ProductCardList products={newArrivals} title='New Arrivals' isLoading={isLoading} />
         {/* Featured Collection Section */}
