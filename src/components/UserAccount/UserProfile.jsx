18a1139a51b79c192e31d34c5f91c19866de911a
ellenpham <ellenpham1510@gmail.com>
2023-12-22 19:18:38 +1100

    feat: create user profile and order history view

--- src/components/UserAccount/UserProfile.jsx
+++ src/components/UserAccount/UserProfile.jsx
@@ -7,37 +7,37 @@ import api from '../../configs/api';
 
 // USER PROFILE COMPONENT
 export const UserProfile = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [user, setUser] = useState([]);
-    
-    useEffect(() => {
-        const fetchProducts = async () => {
-          setIsLoading(true);
-    
-          try {
-            //Call the api.get method to retrieve products.
-            const newArrivalProductsResponse = await api.get('/users/');
-            setUser(newArrivalProductsResponse.data);
-          } catch (error) {
-            // If there is an error during the registration process, catches the error and displays an error message.
-            toast.error('Error fetching products', error);
-          } finally {
-            setIsLoading(false);
-          }
-        };
-    
-        fetchProducts();
-      }, []);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+
+      try {
+        // Call the api.get method to retrieve users
+        const user = await api.get('/auth/current-user');
+        setUser(user.data);
+      } catch (error) {
+        // If there is an error during the registration process, catches the error and displays an error message.
+        toast.error('Error fetching user', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <div className='mt-10'>
       <h1 className='text-3xl mb-8'>My Account</h1>
       <form>
         <div className='flex flex-row items-center gap-2 mb-3'>
-          <Input value='First Name' />
-          <Input value='Last Name' />
+          <Input value={user.firstName} />
+          <Input value={user.lastName} />
         </div>
-        <Input value='Email' className='mb-3' />
+        <Input value={user.email} className='mb-3' />
         <Input placeholder='Phone' className='mb-3' />
         <Input placeholder='Address' className='mb-3' />
         <Input placeholder='City' className='mb-3' />
--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -5,11 +5,12 @@ import React from 'react';
 import Layout from '../layouts/Base';
 import { Tabs, Tab } from '@nextui-org/react';
 import { UserProfile } from '../components/UserAccount/UserProfile';
+import { OrderHistory } from '../components/UserAccount/OrderHistory';
 
 export default function UserAccountPage() {
   return (
     <Layout>
-      <div className='flex w-full flex-col h-screen mt-10 mx-16 md:mx-24'>
+      <div className='flex flex-col h-screen mt-10 w-4/5 mx-auto'>
         <Tabs aria-label='Options' fullWidth size='lg' color='primary' variant='solid'>
           <Tab key='profile' title='User Profile'>
             <UserProfile />
@@ -18,7 +19,7 @@ export default function UserAccountPage() {
             <div></div>
           </Tab>
           <Tab key='orderhistory' title='Order History'>
-            <div></div>
+            <OrderHistory />
           </Tab>
         </Tabs>
       </div>
