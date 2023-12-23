c95dcef445993a3ec44ea3d97eaba9b162dc042b
ellenpham <ellenpham1510@gmail.com>
2023-12-23 19:21:54 +1100

    feat: fix some responsive design

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -10,11 +10,11 @@ export const CartItem = ({ item }) => {
 
   return (
     <div className='border rounded p-4 mb-4 mt-5 border-pink-500 text-lg w-full'>
-      <div className='flex items-center m-auto'>
-        <div className='flex justify-start items-center w-1/2'>
+      <div className='lg:flex items-center m-auto'>
+        <div className='flex justify-start items-center w-full lg:w-1/2'>
           <img src={item.imageUrl} alt='' className='w-32 h-32' />
-          <div className='ml-5'>
-            <p className='justify-self-start mt-8'>{item.name}</p>
+          <div className='lg:ml-5'>
+            <p className='justify-self-start lg:mt-8'>{item.name}</p>
             <div className='text-sm my-1.5'>${item.price}</div>
             <div
               className='hover:underline cursor-pointer text-xs text-stone-700'
@@ -24,7 +24,7 @@ export const CartItem = ({ item }) => {
             </div>
           </div>
         </div>
-        <div className='flex justify-between items-center w-1/3 ml-auto'>
+        <div className='lg:flex justify-between items-center w-1/3 lg:ml-auto pl-32 lg:pl-0'>
           <div className='flex justify-between items-center'>
             <Button
               isIconOnly
@@ -46,7 +46,9 @@ export const CartItem = ({ item }) => {
               {<FiPlus className='h-5 w-5 text-black-500 self-center' />}
             </Button>
           </div>
-          <p>{currencyFormatter((item.price * item.quantity).toFixed(2))}</p>
+          <p className='mt-3 lg:mt-0'>
+            {currencyFormatter((item.price * item.quantity).toFixed(2))}
+          </p>
         </div>
       </div>
     </div>
