f7db98abd7087e68a50c01ddaafd01cfbb7b6c15
ellenpham <ellenpham1510@gmail.com>
2023-12-23 21:22:58 +1100

    feat: improve UI for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,46 @@
 import { Input, Button, Textarea } from '@nextui-org/react';
-import { FaSquareInstagram, FaSquareFacebook } from 'react-icons/fa6';
+import { FaSquareInstagram, FaSquareFacebook, FaSquarePhone, FaLocationDot } from 'react-icons/fa6';
 import { FiMail, FiUser } from 'react-icons/fi';
+import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
     <div className='w-full bg-primary min-h-[300px] py-6'>
-      <div className='container mx-auto p-6 h-full w-full max-w-[1536px] flex-grow flex'>
+      <div className='container mx-auto p-6 h-full w-full w-1/2 flex-grow flex mt-5'>
         {/*About us*/}
-        <div className='flex flex-col md:flex-row gap-5 w-full lg:gap-14'>
-          <div className='flex flex-col gap-4 w-full lg:w-[60%]'>
-            <h3 className='text-secondary text-lg font-semibold'>ABOUT US</h3>
-            <p className='text-white text-justify'>
-              Sakura Pantry is dedicated to delivering the highest quality Japanese ingredients to
-              homes across Australia. We take pride in offering carefully selected items, and we are
-              delighted to assist you in recreating your favorite Japanese dishes with a touch of
-              luxury in the comfort of your own home.
-            </p>
+        <div className='flex flex-col md:flex-row gap-5 w-full lg:gap-24'>
+          <div className='flex flex-col gap-4 w-full lg:w-[50%]'>
+            <h3 className='text-secondary text-2xl font-semibold'>ABOUT US</h3>
+            <div className='text-white text-justify'>
+              <p className='mb-5'>
+                Sakura Pantry is dedicated to delivering the highest quality Japanese ingredients to
+                homes across Australia. We take pride in offering carefully selected items, and we
+                are delighted to assist you in recreating your favorite Japanese dishes with a touch
+                of luxury in the comfort of your own home.
+              </p>
+              <div className='flex items-center gap-2'>
+                <span>
+                  <FaSquarePhone className='w-5 h-5 text-secondary' />
+                </span>
+                <span>05 7564 6679 | 07 7524 7940</span>
+              </div>
+              <div className='flex items-center gap-2'>
+                <span>
+                  <MdEmail className='w-5 h-5 text-secondary' />
+                </span>
+                <span>sakurapantry.au@email.com</span>
+              </div>
+              <div className='flex items-center gap-2'>
+                <span>
+                  <FaLocationDot className='w-5 h-5 text-secondary' />
+                </span>
+                <span>6451 O'connell Meadow, Sydney, NSW 3000 </span>
+              </div>
+            </div>
           </div>
           {/*Contact form*/}
-          <div className='flex w-full flex-col gap-4 lg:w-[40%]'>
-            <h3 className='text-secondary text-lg font-semibold'>CONTACT US</h3>
+          <div className='flex w-full flex-col gap-4 lg:w-[50%]'>
+            <h3 className='text-secondary text-2xl font-semibold'>CONTACT US</h3>
             <Input
               label='Your name'
               type='text'
@@ -35,13 +56,15 @@ export default function Footer() {
             <div className='flex justify-between'>
               <div className='flex'>
                 <a href='https://www.facebook.com/'>
-                  <FaSquareFacebook className='text-white w-11 h-11 mr-4' />
+                  <FaSquareFacebook className='text-secondary w-9 h-9 mr-4' />
                 </a>
                 <a href='https://www.instagram.com/'>
-                  <FaSquareInstagram className='text-white w-11 h-11' />
+                  <FaSquareInstagram className='text-secondary w-9 h-9' />
                 </a>
               </div>
-              <Button color='secondary'>Send</Button>
+              <Button color='secondary' variant='ghost' className='font-semibold w-1/3'>
+                Send Your Enquiry
+              </Button>
             </div>
           </div>
         </div>
